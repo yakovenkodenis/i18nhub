ee7c2f80179453ed8d34cb37058ad77fb4f080af
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2016-11-13 15:49:15 +0200

    Integrate redux and set up redux devtools extention

--- frontend/app/config/store.js
+++ frontend/app/config/store.js
@@ -2,7 +2,7 @@ import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 
 
-let middlawares = [
+let middlewares = [
     thunk
 ];
 
--- frontend/app/index.js
+++ frontend/app/index.js
@@ -1,31 +1,38 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import { BrowserRouter, Match, Link, Miss } from 'react-router';
 
 import MainLayout from './layouts/main';
 import Home from './components/home';
 import About from './components/about';
 
+import store from './config/store';
+import reducers from './reducers';
+
+
+const reduxDevTools =
+    process.env.NODE_ENV !== 'production'
+    && window.__REDUX_DEVTOOLS_EXTENSION__
+    && window.__REDUX_DEVTOOLS_EXTENSION__();
 
 const App = () => (
-    <BrowserRouter>
-        <MainLayout>
+    <Provider store={store(reducers, reduxDevTools)}>
+        <BrowserRouter>
+            <MainLayout>
+                <hr />
+                <ul>
+                    <li><Link to='/'>Home</Link></li>
+                    <li><Link to='/about'>About</Link></li>
+                </ul>
+                <hr />
 
-            <hr />
-
-            <ul>
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='/about'>About</Link></li>
-            </ul>
-
-            <hr />
-
-            <Match exactly pattern='/' component={Home} />
-            <Match pattern='/about' component={About} />
-
-            <Miss component={Home} />
-        </MainLayout>
-    </BrowserRouter>
+                <Match exactly pattern='/' component={Home} />
+                <Match pattern='/about' component={About} />
+                <Miss component={Home} />
+            </MainLayout>
+        </BrowserRouter>
+    </Provider>
 );
 
 render(<App />, document.querySelector('#root'));
